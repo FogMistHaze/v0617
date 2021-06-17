d672ec048612ca83d4eb003f0536f335e5d8ed10
A601-14 <tnk.dat.class@gmail.com>
2021-06-17 11:46:58 +0900

    演習11-1出来ないんだけど？

--- v0617/Form1.cs
+++ v0617/Form1.cs
@@ -24,9 +24,11 @@ namespace v0617
         private void timer1_Tick(object sender, EventArgs e)
         {
             Point spos = MousePosition;
-            spos = PointToClient(spos);
-            label3.Left = spos.X/2;
-            label3.Top = spos.Y/2;
+            Point fpos = PointToClient(spos);
+
+            label3.Left = fpos.X-label3.Width/2;
+            label3.Top = fpos.Y-label3.Height/2;
+            label3.Text = "" + fpos.X + "," + fpos.Y;
 
             label1.Left += vx;
             label1.Top += vy;
@@ -49,8 +51,16 @@ namespace v0617
             }
 
             score--;
-            //惜しかったよ((
             label2.Text = $"Score{score}";
+
+            if ((fpos.X >= label1.Left)
+                && (fpos.X < label1.Left)
+                && (fpos.Y >= label1.Top)
+                && (fpos.Y < label1.Bottom)
+                ) 
+            {
+                timer1.Enabled = false;
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
